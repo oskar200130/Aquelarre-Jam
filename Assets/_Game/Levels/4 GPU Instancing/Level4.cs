9a8521a9b954dd876f5c21176e25f08ddfb5441d
Diego <62880649+diecasti@users.noreply.github.com>
2024-12-16 19:48:08 +0100

    mi ejemplo actualizado

--- Assets/_Game/Levels/4 GPU Instancing/Level4.cs
+++ Assets/_Game/Levels/4 GPU Instancing/Level4.cs
@@ -30,14 +30,30 @@ public class Level4 : MonoBehaviour
         SceneTools.Instance.SetNameText("GPU Instancing");
     }
 
+
+    public Vector3 screenPosition;
+    public Vector3 worldPosition = new Vector3(0, 0, 0);
     private void Update()
     {
         var time = Time.time;
+
+        screenPosition = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hitData))
+        {
+            worldPosition = hitData.point;
+        }
+
+
         for (var i = 0; i < _positions.Length; i++)
         {
-            var (pos, rot) = _positions[i].CalculatePos(_cubeYOffsets[i], time);
+
+            var (pos, rot) = _positions[i].CalculatePosMouse(_cubeYOffsets[i], time, worldPosition);
 
             _matrices[i].SetTRS(pos, rot, SceneTools.CubeScale);
+
 
             _positions[i].y = pos.y;
         }
